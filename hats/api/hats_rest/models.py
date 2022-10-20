71ffe39f02c3057aac8ed649e9ba55312888b984
Rosario <rosariodavi9@gmail.com>
2022-10-19 20:37:35 -0400

    readded poller, views, mods working

--- hats/api/hats_rest/models.py
+++ hats/api/hats_rest/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class LocationVO(models.Model):
+    closet_name = models.CharField(max_length=100)
+    section_number = models.PositiveSmallIntegerField(null = True)
+    shelf_number = models.PositiveSmallIntegerField(null = True)
+    location_id = models.PositiveSmallIntegerField(null=True, unique = True)
+
+
+class Hat(models.Model):
+    style_name = models.CharField(max_length=20)
+    fabric = models.CharField(max_length=20, null=True)
+    color = models.CharField(max_length=20, null=True)
+    picture = models.URLField(null=True)
+    location = models.ForeignKey(
+        LocationVO,
+        related_name="hat_location",
+        on_delete=models.CASCADE,
+        null= True
+    )
--- hats/api/hats_rest/views.py
+++ hats/api/hats_rest/views.py
@@ -1,3 +1,73 @@
 from django.shortcuts import render
+from common.json import ModelEncoder
+from .models import LocationVO, Hat
+from django.http import JsonResponse
+from django.views.decorators.http import require_http_methods
+import json
 
-# Create your views here.
+class LocationVOEncoder(ModelEncoder):
+    model = LocationVO
+    properties = ["closet_name", "section_number", 'shelf_number']
+
+
+class HatListEncoder(ModelEncoder):
+    model = Hat
+    properties = ["style_name"]
+
+class HatDetailEncoder(ModelEncoder):
+    model = Hat
+    properties = [
+        'style_name',
+        'fabric',
+        'color',
+        'picture',
+        'location'
+    ]
+    encoders = {'location': LocationVOEncoder()}
+
+@require_http_methods(["GET", "POST", "DELETE"])
+def api_list_hats(request, location_vo_id=None):
+    if request.method == "GET":
+        if location_vo_id is not None:
+            hats=Hat.objects.filter(location=location_vo_id)
+        else:
+            hats = Hat.objects.all()
+
+        return JsonResponse(
+            {"hats": hats}, encoder = HatListEncoder
+        )
+    elif request.method == "DELETE":
+        count, _ = Hat.objects.filter(id=location_vo_id).delete()
+        return JsonResponse({"deleted": count > 0})
+    else: 
+        content = json.loads(request.body)
+
+        try: 
+            location_href = content["location"]
+            location = LocationVO.objects.get(location_id=location_href)
+            content["location"] = location
+        except LocationVO.DoesNotExist:
+            return JsonResponse(
+                {'message': "Invalid location id"},
+                status=400
+            )
+        
+        location = Hat.objects.create(**content)
+        return JsonResponse(
+            location,
+            encoder=HatDetailEncoder,
+            safe=False
+        )
+
+
+def api_show_hats(request, pk):
+    hat = Hat.objects.get(id=pk)
+    return JsonResponse(
+        hat,
+        encoder=HatDetailEncoder,
+        safe = False,
+
+
+    )
+
+
