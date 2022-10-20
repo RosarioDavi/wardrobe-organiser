1847e9c7b47076b206e2dc3a87c02b2160f2a723
Woojae <joki4127@gmail.com>
2022-10-19 18:16:13 -0600

    Added shoes models

--- shoes/api/shoes_rest/models.py
+++ shoes/api/shoes_rest/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.forms import URLField
 
 # Create your models here.
+class BinVO(models.Model):
+    closet_name = models.CharField(max_length=100)
+    import_href = models.CharField(max_length=200, unique=True)
+
+class Shoe(models.Model):
+    shoe_name = models.CharField(max_length=100)
+    color = models.CharField(max_length=50, null=True)
+    picture_url = models.URLField(null=True, blank=True)
+    bin = models.ForeignKey(
+        BinVO, 
+        related_name ="shoe",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True
+    )
